44d8abae5c2544c0829aeb9616bd1a73f855c9a3
Michael Berber <mickberber@icloud.com>
2017-02-22 10:58:11 -0800

    add dotenv for variables, add compression

--- server/middleware.js
+++ server/middleware.js
@@ -1,9 +1,13 @@
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const compression = require('compression');
 const utils = require('./utils');
 
 module.exports = (app, express) => {
   app.use(morgan('dev'));
+
+  app.use(compression());
+
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({
     extended: true,
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const path = require('path');
 const middleware = require('./middleware');
 
+require('dotenv').config();
+
 middleware(app, express);
 
 app.use('/', express.static(path.join(__dirname, '../public')));
@@ -12,6 +14,8 @@ app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-const server = app.listen(3000, () => {
+const PORT = process.env.PORT;
+
+const server = app.listen(PORT, () => {
   console.log('listening at ' + server.address().port + '....')
 });
