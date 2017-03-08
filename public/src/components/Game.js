194a1723e683b4786dfb69001c50fbbba83232d2
Michael Berber <mickberber@icloud.com>
2017-03-07 16:39:48 -0800

    complete basic reqs

--- public/src/components/Game.js
+++ public/src/components/Game.js
@@ -4,11 +4,17 @@ import styles from '../styles/main.css';
 const Game = ({game, selected}) => {
   return (<div className={styles.game_thumb}
               id={selected ? styles.selected : ''}>
-    <div>{game.away_team_name + ' vs. ' + game.home_team_name}</div>
-    <img src={game.video_thumbnails.thumbnail[0].content} />
-    <div>{game.time + ' ' + game.time_zone}</div>
-    <div>{game.venue}</div>
-    <div>{game.location}</div>
+    <div className={styles.game_title}>
+      {game.away_team_name + ' vs. ' + game.home_team_name}
+    </div>
+    <div className={styles.game_img}>
+      <img src={game.video_thumbnails.thumbnail[0].content} />
+    </div>
+    <div className={styles.game_details}>
+      <div>{game.time + ' ' + game.time_zone}</div>
+      <div>{game.venue}</div>
+      <div>{game.location}</div>
+    </div>
   </div>)
 }
 
